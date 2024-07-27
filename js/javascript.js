aba2a65011e3564a8e8323be2cb53446acd76757
lochpm <163648572+lochpeon@users.noreply.github.com>
2024-07-27 17:48:36 +0800

    Create createCanvas, removeCanvas functions
    Create eventListener for grid-reset button that calls createCanvas
    Create createCanvas, removeCanvas functions for generation of sketch canvas pieces
    - createCanvas generates the sketch grid items based on the received number of sides,
    - removeCanvas removes all generated items on the sketch grid beforehand
    Create a css variable to pass the JavaScript value to the stylesheet

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,28 @@
 const canvas = document.querySelector("main");
 const btn = document.querySelector(".grid-reset");
 
-for(let i = 0; i < 256; i++) {
-    const piece = document.createElement("div");
-    piece.className = "piece";
+function createCanvas(sides) {
+    let proportion = 100 / sides;
+    let totalSides = sides ** 2;
+
+    removeCanvas();
+
+    for(let i = 0; i < totalSides; i++) {
+        const piece = document.createElement("div");
+        piece.className = "piece";
+        
+        canvas.appendChild(piece);
+    }
+
+    document.documentElement.style.setProperty(`--proportion`, `${proportion}%`);
+}
+
+function removeCanvas() {
+    const canvasPiece = document.querySelectorAll("div");
     
-    canvas.appendChild(piece);
+    canvasPiece.forEach((piece) => {
+        canvas.removeChild(piece);
+    });
 }
 
 canvas.addEventListener("mouseenter", () => {
@@ -19,3 +36,14 @@ canvas.addEventListener("mouseenter", () => {
 
     btn.classList.remove("invisible");
 });
+
+btn.addEventListener("mousedown", () => {
+    let response = 0;
+    do {
+        response = +prompt("How many squares per side of the new grid would you like? We prefer less than or equal to 100.", 0);
+    } while (response > 100 || response < 1 || Object.is(response, NaN));
+
+    createCanvas(response);
+});
+
+createCanvas(16);
