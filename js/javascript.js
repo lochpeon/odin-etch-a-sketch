c63a317a13a9a970b17c331e6397ae7fabe16d41
lochpm <163648572+lochpeon@users.noreply.github.com>
2024-07-27 16:05:43 +0800

    Create eventListener to color the grid on hover
    Create two(2) eventListener on mouse hover for main, divs that will add a class to color the hovered div
    Create style rule for .hovered class
    Replace variable name on creation of sketch grid pieces

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,19 @@
 const canvas = document.querySelector("main");
 
 for(let i = 0; i < 256; i++) {
-    const canvasPiece = document.createElement("div");
-    canvasPiece.className = "piece";
+    const piece = document.createElement("div");
+    piece.className = "piece";
     
-    canvas.appendChild(canvasPiece);
+    canvas.appendChild(piece);
 }
+
+canvas.addEventListener("mouseenter", () => {
+    const canvasPiece = document.querySelectorAll("div");
+
+    canvasPiece.forEach((piece) => {
+        piece.addEventListener("mouseenter", () => {
+            console.log("hovered");
+            piece.classList.add("hovered");
+        });
+    }); 
+});
